4833527baaac92f82fcdd382e919933b4f104b38
Gill Hawkes <gillhawkes1@gmail.com>
2024-07-16 11:01:09 -0400

    hook test 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello world deploy test</p>
+      <p>hello world deploy test2</p>
     </main>
   );
 }
