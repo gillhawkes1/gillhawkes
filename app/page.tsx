7f8760fa610df28143345ed14a96ed3c14d9b450
Gill Hawkes <gillhawkes1@gmail.com>
2024-09-14 20:58:39 -0400

    angular app start

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello world deploy test2</p>
-    </main>
-  );
-}
