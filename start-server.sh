8989a5af02c8512a26a049fc6313a4b4544ab076
Gill Hawkes <gillhawkes1@gmail.com>
2024-09-15 22:30:25 -0400

    updated server start

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /root/.nvm/versions/node/v22.2.0/bin/http-server /var/www/gillhawkes.com/public_html/gillhawkes/dist/gillhawkes/browser -p 3002 -P http://localhost:3002 -c-1
+echo "server started"
