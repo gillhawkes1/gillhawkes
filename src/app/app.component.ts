198aac11649fcbd52abed35e38849df94c4d40cf
Gill Hawkes <gillhawkes1@gmail.com>
2024-09-16 14:11:56 -0400

    updated nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { version } from '../../package.json';
+import { AboutmeComponent } from './pages/aboutme/aboutme.component';
+import { ContactmeComponent } from './pages/contactme/contactme.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, RouterLink, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
