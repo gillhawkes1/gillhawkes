ec8f59697e22545b38d7ee3b7897c5a077a534a5
Gill Hawkes <gillhawkes1@gmail.com>
2024-09-29 12:44:52 -0400

    updated start server script

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/root/.nvm/versions/node/v22.2.0/bin/http-server /var/www/gillhawkes.com/public_html/gillhawkes/dist/gillhawkes/browser -p 3002 -P http://localhost:3002 -c-1
+/root/.nvm/versions/node/v22.2.0/bin/http-server /var/www/gillhawkes.com/public_html/gillhawkes/dist/gillhawkes/browser -p 3002 -P http://localhost:3002 -c-1 -S -C /etc/ssl/certs/ssl-cert-snakeoil.pem -K /etc/ssl/private/ssl-cert-snakeoil.key
 echo "server started"
