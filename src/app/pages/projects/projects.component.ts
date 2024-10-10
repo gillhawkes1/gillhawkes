46083721646076b4cb8fdd3bc664ca485fb29e4b
Gill Hawkes <gillhawkes1@gmail.com>
2024-10-09 21:41:26 -0400

    added ngondestroy for github call

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GithubService } from '../../services/github.service';
 import { NgFor, CommonModule } from '@angular/common';
 import { RepoComponent } from '../../components/repo/repo.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
@@ -14,15 +15,22 @@ import { RepoComponent } from '../../components/repo/repo.component';
 export class ProjectsComponent {
   title = 'My Projects'
   repos: any[] = [];
+  private subscription: Subscription | null = null;
 
   constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
-    this.githubService.getRepos().subscribe((data) => {
+    this.subscription = this.githubService.getRepos().subscribe((data) => {
       this.repos = data.filter((repo) => {
         return this.showProjects.includes(repo.name);
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   showProjects: string[] = [
